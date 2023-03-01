5cc1e2b2df01d0b89ee5f8e21dfbe553ab11453b
ecdns <clementpavot78@gmail.com>
2023-03-01 16:02:19 +0000

    update

--- gamesStats.py
+++ gamesStats.py
@@ -90,6 +90,10 @@ def read_line_score(soup):
     cols[-1] = "total"
     line_score.columns = cols
     return line_score
+# Lire les stats d'une des deux équipes de la rencontre
+def read_stats(soup, team, stat):
+    df = pd.read_html(str(soup), attrs={"id": f"box-{team}-game-{stat}"}, index_col=0)[0]
+    return df
 
 def update():
     asyncio.run(scrape_season())
@@ -99,3 +103,36 @@ def update():
         filepath = os.path.join(STANDINGS_DIR, f)
         print(filepath)
         asyncio.run(scrape_game(filepath))
+
+base_cols = None
+box_score = box_scores[0]
+print(box_score)
+soup = parse_html(box_score)
+line_score = read_line_score(soup)
+teams = list(line_score["team"])
+
+summaries = []
+for team in teams:
+    basic = read_stats(soup, team, "basic")
+    advanced = read_stats(soup, team, "advanced")
+    totals = pd.concat([basic.iloc[-1,:], advanced.iloc[-1,:]]) 
+    totals.index = totals.index.str.lower()
+
+    # Maximums de chacunes des colonnes des tableaux basic et advance 
+    maxes = pd.concat([basic.iloc[:-1,:].max(), advanced.iloc[:-1,:].max()])
+    maxes.index = maxes.index.str.lower() + "_max"
+    print(maxes)
+
+    summary = pd.concat([totals, maxes])
+
+    if(base_cols is None):
+        # Supprime les colonnes qui sont identiques (mp)
+        base_cols = list(summary.index.drop_duplicates(keep="first"))
+        # bpm est dans certaines box scores mais pas dans d'autres
+        base_cols = [b for b in base_cols if "bpm" not in b]
+
+    summary = summary[base_cols]
+    summaries.append(summary)
+summary = pd.concat(summaries, axis=1).T
+
+game = pd.concat([summary, line_score], axis=1)
